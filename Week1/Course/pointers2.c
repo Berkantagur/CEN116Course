802d4101e0e410c9de52f403cb0066756dd1ad8c
Berkantagur <berkantagur6@gmail.com>
2023-03-27 13:57:35 +0300

    .py

---
+++ Week1/Course/pointers2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Cube a variable using Pass-By-Value
+/*
+int cubeByValue(int n);
+
+int main(void){
+
+    int number = 6; //Initialize number
+
+    printf("The original value of number is %d", number);
+
+    // Pass number by value to cubeByValue
+    number = cubeByValue(number);
+
+    printf("\nThe new value of number is %d\n", number);
+
+}
+
+// Calculate and return cube of integer argument
+int cubeByValue(int n){
+
+    return n * n * n; // Cube local variable n and return result
+
+}
+*/
