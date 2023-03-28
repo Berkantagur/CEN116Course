8a3d62dd7dd96b1435ac828add24f025d1e48f25
Berkantagur <berkantagur6@gmail.com>
2023-03-28 12:36:21 +0300

    .c

---
+++ Week1/Course/pointersLab2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// pbv --> pass by value
+// pbr --> pass by reference 
+
+void pbr(int *n);
+int pbv(int n);
+
+int main(void) {
+
+    int number = 9;
+    printf("Number: %d\n", number);
+    pbr(&number);
+    printf("Number: %d\n", number);
+
+    return 0;
+}
