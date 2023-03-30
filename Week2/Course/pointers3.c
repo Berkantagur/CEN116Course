95a73b0395ee7e450c20efd6099db08c85b5b3f2
Berkantagur <berkantagur6@gmail.com>
2023-03-30 11:19:42 +0300

    .c

--- Week2/Course/pointers3.c
+++ Week2/Course/pointers3.c
@@ -3,8 +3,9 @@
 
 int main(){
 
+    int arr[] = {1, 2, 3, 4}; //initialize array arr
+    int *arrPtr = arr; // set arrPtr to point to array arr
+    int i; // counter
+    int offset; //counter
 
-
-
-    
 }
