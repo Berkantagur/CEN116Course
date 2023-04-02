7ccc58e572c3389eed059e78c7fa4d4f83276a0e
Berkantagur <berkantagur6@gmail.com>
2023-04-02 16:56:55 +0300

    .c

--- Week2/Practices/pointers5.c
+++ Week2/Practices/pointers5.c
@@ -21,3 +21,22 @@ void substraction(int n1, int n2)
         printf("Result: %d", result);
     }
 }
+
+void multiply(int n1, int n2)
+{
+    result = n1 * n2;
+    printf("Result: %d", result);
+}
+
+void division(int n1, int n2)
+{
+    if(n1 >= n2)
+    {
+        result = n1 / n2;
+        printf("Result: %d", result);
+    }
+    else{
+        result = n2 / n1;
+        printf("Result: %d", result);
+    }
+}
