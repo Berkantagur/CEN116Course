34087ae0f43bdce949600c879cfc36c9f109ab0f
Berkantagur <berkantagur6@gmail.com>
2023-04-10 23:17:23 +0300

    .c

--- Week3/Course/stings4.c
+++ Week3/Course/stings4.c
@@ -4,6 +4,7 @@
 int main(void){
 
     printf("%s\n%s%s%s\n%s%s%s\n%s%s\n\n", "According to isspace:",
+
         "Newline", isspace('\n')    ? " is a " : " is not a ",
         "whitespace character", "Horizontal tab",
         isspace('\t')   ? " is a " : " is not a ",
@@ -12,11 +13,13 @@ int main(void){
         "whitespace character");
 
     printf("%s\n%s%s%s\n%s%s\n\n", "According to iscntrl:",
+
         "Newline", iscntrl('\n')    ? " is a " : " is not a ",
         "control character", iscntrl('$')   ? "$ is a " :
         "$ is not a ", "control character");
 
     printf("%s\n%s%s\n%s%s\n%s%s\n\n", "According to ispunct:",
+
         ispunct(';')    ? "; is a " : "; is not a ",
         "punctuation character",
         ispunct('Y')    ? "Y is a " : "Y is not a ",
@@ -25,12 +28,14 @@ int main(void){
         "punctuation character");
 
     printf("%s\n%s%s\n%s%s%s\n\n", "According to isprint:",
+
         isprint('$')    ? "$ is a " : "$ is not a ",
         "printing character",
         "Alert", isprint('\a')  ? " is a " : " is not a ",
         "printing character");
 
     printf("%s\n%s%s\n%s%s%s\n", "According to isgraph:",
+    
         isgraph('Q')    ? "Q is a " : "Q is not a ",
         "printing character other than a space",
         "Space", isgraph(' ')   ? " is a " : " is not a ",
