660eabd92ee6fc9063fc81d2751de55cbb1454c2
Berkantagur <berkantagur6@gmail.com>
2023-04-11 15:22:29 +0300

    .c

--- Week4/Course/strings2.1.c
+++ Week4/Course/strings2.1.c
@@ -6,17 +6,20 @@
 
 int main(void){
 
-    char x[] = "Happy Birthday to You";
-    char y[SIZE1];
-    char z[SIZE2];
+    char x[] = "Happy Birthday to You"; // initialize char array x
+    char y[SIZE1]; // create char array y
+    char z[SIZE2]; // create char array z
 
+    // copy contents of x into y
     printf("%s%s\n%s%s\n",
         "The string in array x is: ", x,
         "The string in array y is: ", strcpy(y, x));
 
+    // copy first 14 characters of x into z. Does not copy null
+    // character
     strncpy(z, x, SIZE2 - 1);
 
-    z[SIZE2 -1] = '\0';
+    z[SIZE2 -1] = '\0'; // terminate string in z
     printf("The string in array z is: %s\n", z);
 
 }
