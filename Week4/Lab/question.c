db2bbfdbf5d257224de6eebac149bc4ba7f9b760
Berkantagur <berkantagur6@gmail.com>
2023-04-16 14:28:56 +0300

    .c

--- Week4/Lab/question.c
+++ Week4/Lab/question.c
@@ -18,12 +18,21 @@ int main(void){
         printf("%c is an lowercase letter.\n", ch);
      
 
-        return 0;
+    return 0;
 }
 
 int isupper(char ch){
 
     if(ch >= 'A' && ch <= 'Z')
+        return 1;
+    
+    else 
+        return 0;
+}
+
+int islower(char ch){
+
+    if(ch >= 'a' && ch <= 'z')
         return 1;
     
     else 
