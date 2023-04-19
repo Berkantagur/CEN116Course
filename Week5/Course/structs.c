792d9d0fb7c7f0891292c524c5b439deff4cea3e
Berkantagur <berkantagur6@gmail.com>
2023-04-19 22:16:06 +0300

    .c

--- Week5/Course/structs.c
+++ Week5/Course/structs.c
@@ -10,3 +10,15 @@ int scan_complex(complex_t *c);
 void print_complex(complex_t);
 complex_t add_complex(complex_t c1, complex_t c2);
 complex_t subtract_complex(complex_t c1, complex_t c2);
+
+int main(void){
+
+    complex_t com1, com2;
+
+    printf("Enter the real and imaginary parts of a complex number\n");
+    printf("separated by a space> ");
+    scan_complex(&com1);
+    printf("Enter a second complex number> ");
+    scan_complex(&com2);
+
+}
